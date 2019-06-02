56b0138da195ab8ffa4183d79c7308ae3cac9df5
waheedmusa <abdulwaheedmusa0@gmail.com>
2019-06-02 19:47:46 +0300

    changes made

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function trackResult(){
 
-  var side1 = parseInt(document.getElementById('side1').value);
-  var side2 = parseInt(document.getElementById('side2').value);
-  var side3 = parseInt(document.getElementById('side3').value);
-  var result = document.getElementById('result');
-  var pass = function(side1,side2,side3){
-   return side1+side2>side3 && side2+side3>side1 && side3+side1>side2
+var side1 = parseInt(document.getElementById('side1').value);
+var side2 = parseInt(document.getElementById('side2').value);
+var side3 = parseInt(document.getElementById('side3').value);
+var result = document.getElementById('result');
+var pass = function(side1,side2,side3){
+  return side1+side2>side3 && side2+side3>side1 && side3+side1>side2
 }
 if(side1===side2 && side2===side3){
 result.textContent=("This is a equilateral triangle.");
@@ -13,8 +13,8 @@ result.textContent=("This is a equilateral triangle.");
 else if(side1 === side2 || side1 === side3 || side2 === side3){
 result.textContent=("This is an isosceles.");
 }
-else if(side1 === side2 || side1 === side3 || side2 === side3){
-result.textContent=("This is an isosceles.");
+else if(side1>=side2+side3 || side2>=side1+side3 || side3>=side1+side3){
+result.textContent=("This is not a triangle.");
 }
 else if(isNaN(side1) || isNaN(side2) || isNaN(side3)){
 result.textContent=("Please input values.");
