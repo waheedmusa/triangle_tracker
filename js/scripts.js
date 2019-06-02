388426f1fe7689e0338c7eaf54651f0be0371b63
waheedmusa <abdulwaheedmusa0@gmail.com>
2019-06-02 19:18:49 +0300

    pass variable added

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,7 @@ function trackResult(){
   var side2 = parseInt(document.getElementById('side2').value);
   var side3 = parseInt(document.getElementById('side3').value);
   var result = document.getElementById('result');
+  var pass = function(side1,side2,side3){
+  return side1+side2>side3 && side2+side3>side1 && side3+side1>side2
+}
 }
