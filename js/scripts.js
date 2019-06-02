56c168f0bde5d8294b5d2eb01e88f55cb1e04102
waheedmusa <abdulwaheedmusa0@gmail.com>
2019-06-02 19:17:55 +0300

    variables added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 function trackResult(){
-  
+
+  var side1 = parseInt(document.getElementById('side1').value);
+  var side2 = parseInt(document.getElementById('side2').value);
+  var side3 = parseInt(document.getElementById('side3').value);
+  var result = document.getElementById('result');
 }
