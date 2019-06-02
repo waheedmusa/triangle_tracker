cdf5cd0c246a4824f67f56eda0fbbee5925852fa
waheedmusa <abdulwaheedmusa0@gmail.com>
2019-06-02 19:19:56 +0300

    if statement for equilateral triangle added

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,9 @@ function trackResult(){
   var side3 = parseInt(document.getElementById('side3').value);
   var result = document.getElementById('result');
   var pass = function(side1,side2,side3){
-  return side1+side2>side3 && side2+side3>side1 && side3+side1>side2
+   return side1+side2>side3 && side2+side3>side1 && side3+side1>side2
+}
+if(side1===side2 && side2===side3){
+result.textContent=("This is a equilateral triangle.");
 }
 }
